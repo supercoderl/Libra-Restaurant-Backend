5f39c4541de4697d3e9818f6fa864b4d04e6aaaa
supercoderl <150410325+supercoderl@users.noreply.github.com>
2024-08-18 10:58:27 +0700

    payment

---
+++ LibraRestaurant.Application/Interfaces/IPaymentService.cs
@@ -0,0 +1,34 @@
+﻿using LibraRestaurant.Application.ViewModels.Payments;
+using Stripe.Checkout;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraRestaurant.Application.Interfaces
+{
+    public interface IVnPayService
+    {
+        Task<string> Pay(CreateVNPayViewModel request);
+    }
+
+    public interface IPaypalService
+    {
+        Task<CreateOrderResponse?> CreateOrder(CreateOrderRequest request);
+        Task<CaptureOrderResponse?> CaptureOrder(string orderId);
+        Task<string> GetTransactions();
+    }
+
+/*    public interface IPayOsService
+    {
+        Task<Response<CreatePaymentResult>> CreateOrderPayOS(CreatePayOSViewModel request);
+        Task<Response<PaymentLinkInformation>> CancelOrder(long orderID);
+    }*/
+
+    public interface IStripeService
+    {
+        Task<Session> CreateOrderStripe(SessionStripe request);
+        Task<Session> RetrieveSession(string id);
+    }
+}
---
+++ LibraRestaurant.Application/ViewModels/Payments/CreateStripeViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraRestaurant.Application.ViewModels.Payments
+{
+    public class SessionStripe
+    {
+        public long Amount { get; set; }
+        public string Currency { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public int Quantity { get; set; }
+        public string Mode { get; set; } = string.Empty;
+        public string? CustomerEmail { get; set; }
+    }
+}
